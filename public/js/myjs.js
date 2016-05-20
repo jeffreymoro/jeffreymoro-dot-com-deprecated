11da0bf6c7e385e7c82cfc445175ef70ddc46f69
Jeffrey Moro <jtm0624@me.com>
2016-05-20 14:24:01 -0400

    changes associated w/ moving cv.html to a combo of Liquid template + YAML

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -22,4 +22,85 @@ $(document).ready(function(){
     $("#showheaderhidden1").click(function(){
         $("#headerhidden1").slideToggle();
     });
+  
+    $("#showEmployment1").click(function(){
+        $("#employment1").slideToggle();
+    });
+  
+  
+    $("#showProject1").click(function(){
+        $("#project1").slideToggle();
+    });
+  
+    $("#showProject2").click(function(){
+        $("#project2").slideToggle();
+    });
+  
+    $("#showProject3").click(function(){
+        $("#project3").slideToggle();
+    });
+  
+    $("#showProject4").click(function(){
+        $("#project4").slideToggle();
+    });
+  
+    $("#showProject5").click(function(){
+        $("#project5").slideToggle();
+    });
+  
+    $("#showProject6").click(function(){
+        $("#project6").slideToggle();
+    });
+  
+    $("#showProject7").click(function(){
+        $("#project7").slideToggle();
+    });
+  
+    $("#showProject8").click(function(){
+        $("#project8").slideToggle();
+    });
+  
+    $("#showProject9").click(function(){
+        $("#project9").slideToggle();
+    });
+  
+  
+    $("#showCourse1").click(function(){
+        $("#course1").slideToggle();
+    });
+  
+    $("#showCourse2").click(function(){
+        $("#course2").slideToggle();
+    });
+  
+    $("#showCourse3").click(function(){
+        $("#course3").slideToggle();
+    });
+  
+  
+    $("#showAward1").click(function(){
+        $("#award1").slideToggle();
+    });
+  
+    $("#showAward2").click(function(){
+        $("#award2").slideToggle();
+    });
+  
+    $("#showAward3").click(function(){
+        $("#award3").slideToggle();
+    });
+  
+    $("#showAward4").click(function(){
+        $("#award4").slideToggle();
+    });
+  
+    $("#showAward5").click(function(){
+        $("#award5").slideToggle();
+    });
+  
+    $("#showAward6").click(function(){
+        $("#award6").slideToggle();
+    });
+  
 });
+
